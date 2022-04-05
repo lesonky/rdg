400948e5021c4250eb3d0bbccd55eb60f5cf1407
shaojiasong <lesonky@163.com>
2022-04-05 21:06:33 +0800

    add lock form script

--- vite.config.ts
+++ vite.config.ts
@@ -11,6 +11,7 @@ import { ArcoResolver } from 'unplugin-vue-components/resolvers';
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  base: '/rdgdemo/',
   resolve: {
     alias: {
       '@': path.resolve(__dirname, 'src'),
