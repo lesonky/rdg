5de767ec85261a110cbdea80cfe8a8cf3d2db991
shaojiasong <lesonky@163.com>
2022-04-05 22:02:53 +0800

    add dist

--- vite.config.ts
+++ vite.config.ts
@@ -11,7 +11,7 @@ import { ArcoResolver } from 'unplugin-vue-components/resolvers';
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: '/rdg/',
+  base: '/rdg/dist/',
   resolve: {
     alias: {
       '@': path.resolve(__dirname, 'src'),
